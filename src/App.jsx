23212aa8b55290833d3f61c8cfa7f5b3be336f03
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-22 23:04:38 +0200

    Aggiorna README e implementa la funzionalità di caricamento delle attrici in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const url = "https://lanciweb.github.io/demo/api/actresses/";
+  const [charactersDate, setCharactersDate] = useState(null);
 
-  return <></>;
+  function handleClick() {
+    console.log("fetch date");
+    fetch(url)
+      .then((res) => res.json())
+      .then((date) => {
+        console.log(date);
+        setCharactersDate(date);
+      });
+  }
+
+  return (
+    <>
+      <h1>Attrici</h1>
+      <button onClick={handleClick}>Carica lista Attrici</button>
+    </>
+  );
 }
 
 export default App;
