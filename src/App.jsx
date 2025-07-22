a39b20e2243dfeaec5acf55c8a7c7c7e73bf53a5
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-23 00:31:58 +0200

    Ristruttura il layout del componente App e migliora la visualizzazione delle attrici

--- src/App.jsx
+++ src/App.jsx
@@ -16,33 +16,47 @@ function App() {
 
   return (
     <>
-      <h1>Attrici</h1>
-      <button onClick={handleClick}>Carica lista Attrici</button>
-      <hr />
-      <section className="characters">
+      <header className="bg-secondary">
+        <div className="container p-3">
+          <h1
+            className="text-center
+         me-3 me-3"
+          >
+            <i className="bi bi-camera-reels me-3 text-info"></i>Attrici
+          </h1>
+          <button className="bg-primary text-light" onClick={handleClick}>
+            Carica lista Attrici
+          </button>
+        </div>
+      </header>
+
+      <section className="characters bg-danger p-3 ">
         <div className="container">
           <div className="row">
             {charactersDate &&
               charactersDate?.map((characters, index) => {
                 return (
-                  <div key={characters.id || index} className="col">
-                    <div className="card">
+                  <div key={characters.id || index} className="col-4 mb-4">
+                    <div className="card bg-secondary h-100  ">
                       <img
-                        className="card-img-top"
+                        className="card-img-top  "
                         src={characters.image}
                         alt={characters.name}
+                        style={{ height: "300px", objectFit: "cover" }}
                       />
-                      <h4>{characters.name}</h4>
-                      <p>
-                        <strong>Anno di nascita:</strong>{" "}
-                        {characters.birth_year}
-                      </p>
-                      <p>
-                        <strong>Nazionalità:</strong> {characters.nationality}
-                      </p>
-                      <p>
-                        <strong>Premi:</strong> {characters.awards}
-                      </p>
+                      <div className="p-3 text-light">
+                        <h4>{characters.name}</h4>
+                        <p>
+                          <strong>Anno di nascita:</strong>{" "}
+                          {characters.birth_year}
+                        </p>
+                        <p>
+                          <strong>Nazionalità:</strong> {characters.nationality}
+                        </p>
+                        <p>
+                          <strong>Premi:</strong> {characters.awards}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 );
